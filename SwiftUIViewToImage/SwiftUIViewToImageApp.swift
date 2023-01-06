913ea1c58d8b72a05e32b407cd51720b7d48439e
ketyung <ketyung@techchee.com>
2023-01-06 10:25:18 +0800

    Initial Commit

---
+++ SwiftUIViewToImage/SwiftUIViewToImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewToImageApp.swift
+//  SwiftUIViewToImage
+//
+//  Created by Christopher Chee on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewToImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
